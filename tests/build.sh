91b266e40ad41906e10cd2cd36c9e9c5c43a3df1
tim lindner <tlindner@macmess.org>
2024-06-01 21:14:43 -0700

    more tests

--- tests/build.sh
+++ tests/build.sh
@@ -8,3 +8,4 @@ lwasm --decb -o 4.bin --list=4.lst 4.asm
 lwasm --decb -o 5.bin --list=5.lst 5.asm
 lwasm --decb -o 6.bin --list=6.lst 6.asm
 lwasm --decb -o 7.bin --list=7.lst 7.asm
+lwasm --decb -o 8.bin --list=8.lst 8.asm
